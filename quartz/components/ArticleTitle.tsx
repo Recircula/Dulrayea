b2c5eb1f40e18090367fe3aecb6051119e652633
khuardub <kevendube1@hotmail.com>
2024-05-29 14:47:02 -0400

    Quartz sync: May 29, 2024, 2:47 PM

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -1,21 +1,23 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import { classNames } from "../util/lang"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types";
+import { classNames } from "../util/lang";
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
-  const title = fileData.frontmatter?.title
+  const title = fileData.frontmatter?.title;
   if (title) {
-    <h1 className={classNames(displayClass, "article-title")}>
-    {title.replace(/^\d+\.\s/, '')}
-  </h1>
+    return (
+      <h1 className={classNames(displayClass, "article-title")}>
+        {title.replace(/^\d+\.\s/, '')}
+      </h1>
+    );
   } else {
-    return null
+    return null;
   }
-}
+};
 
 ArticleTitle.css = `
 .article-title {
   margin: 2rem 0 0 0;
 }
-`
+`;
 
-export default (() => ArticleTitle) satisfies QuartzComponentConstructor
+export default (() => ArticleTitle) as QuartzComponentConstructor;
