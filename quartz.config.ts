9f216eba46c1d023e243a660d3113224dfa5b529
Recircula <61711938+Recircula@users.noreply.github.com>
2024-07-21 00:55:46 -0400

    Quartz sync: Jul 21, 2024, 12:55 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,9 +22,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Baskervville SC",
+        body: "Baskervville SC",
+        code: "Baskervville SC",
       },
       colors: {
         lightMode: {
